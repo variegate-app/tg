c7acbcc72a4d9892cf84d8271ee25894494940f4
Konstantin Kukharev <i@kkukharev.ru>
2025-02-08 12:25:18 +0300

    test release 6

--- d.sh
+++ d.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # Проверяем существование папки ./cmd/
-if [ ! -d "./cmd" ]; then
-    echo "Error: Directory ./cmd does not exist."
-    exit 1
-fi
+# Проверяем существование папки ./cmd/
+          if [ ! -d "./cmd" ]; then
+            echo "Error: Directory ./cmd does not exist."
+            exit 1
+          fi
 
-# Используем find для получения только директорий в ./cmd/
-dirs=$(find ./cmd -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
-if [ -z "$dirs" ]; then
-    echo "Error: No directories found in ./cmd/"
-    exit 1
-fi
+          # Используем find для получения только директорий в ./cmd/, преобразуем результат в строку через пробелы
+          dirs=$(find ./cmd -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
+          if [ -z "$dirs" ]; then
+            echo "Error: No directories found in ./cmd/"
+            exit 1
+          fi
 
-# Записываем результат в переменную окружения
-echo "dirs=$dirs" >> $TTT
-echo $dirs
+          dirs=$(echo $dirs | tr '\r' ' ';)
+
+          # Записываем результат в переменную окружения
+          echo "dirs=$dirs"
